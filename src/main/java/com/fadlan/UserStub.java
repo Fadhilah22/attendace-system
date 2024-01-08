17767e8c33628e4b7ad35b1a8ae2b8f60573feff
Fadhilah22 <Fadhlanr22@gmail.com>
2024-01-08 14:22:06 +0700

    First Commit

---
+++ src/main/java/com/fadlan/UserStub.java
@@ -0,0 +1,45 @@
+package com.fadlan;
+
+import java.sql.Timestamp;
+
+public class UserStub{
+    private String id;
+    private String name;
+    private String email;
+
+    public UserStub(){
+
+    }
+
+    // getter
+    public String getId(){
+        return this.id;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+
+    // Setter
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void test () {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
